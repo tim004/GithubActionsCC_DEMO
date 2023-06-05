b19c5dae199eda914d276c3c1065c0f68744944a
Tim <16382637+tim004@users.noreply.github.com>
2023-06-05 11:03:52 +0200

    Update example_test.py

--- tests/unit_tests/example_test.py
+++ tests/unit_tests/example_test.py
@@ -1,8 +1,8 @@
-# import sys    
+import sys    
 
 def test_always_passes():
     print("test")
     assert True
     
-# def test_python_version():
-#     assert sys.version_info.minor > 6
+def test_python_version():
+    assert sys.version_info.minor >= 8
